19205695d1b1dd93af7a7e35cc99d3e61b5a5ab5
Diego18oo <diegorodriguez300806@gmail.com>
2025-11-22 16:16:11 -0600

    Funciona el login y registarse, ya se comunica con el server

---
+++ src/chatcliente/ChatCliente.java
@@ -0,0 +1,16 @@
+
+package chatcliente;
+
+import javax.swing.SwingUtilities;
+
+
+public class ChatCliente {
+
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new LoginUI().setVisible(true);
+        });
+    }
+    
+}
